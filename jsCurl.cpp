215dd45a185245a2fc2fbed75ab7859e9322e6ed
ericn <ericn>
2002-09-29 17:36:23 +0000

    -Initial import

---
+++ jsCurl.cpp
@@ -0,0 +1,185 @@
+/*
+ * Module jsCurl.cpp
+ *
+ * This module defines ...
+ *
+ *
+ * Change History : 
+ *
+ * $Log: jsCurl.cpp,v $
+ * Revision 1.1  2002-09-29 17:36:23  ericn
+ * -Initial import
+ *
+ *
+ * Copyright Boundary Devices, Inc. 2002
+ */
+
+
+#include "jsCurl.h"
+#include "js/jsstddef.h"
+#include "js/jscntxt.h"
+#include "js/jsapi.h"
+#include "js/jslock.h"
+#include "urlFile.h"
+#include <stdio.h>
+
+enum jsCurl_tinyid {
+   CURLFILE_ISOPEN, 
+   CURLFILE_SIZE, 
+   CURLFILE_DATA, 
+   CURLFILE_URL, 
+   CURLFILE_HTTPCODE, 
+   CURLFILE_FILETIME, 
+   CURLFILE_MIMETYPE
+};
+
+extern JSClass jsCurlClass_ ;
+
+static JSBool
+jsCurl_getProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
+{
+printf( "getting property for jsCurl object\n" );
+   if( JSVAL_IS_INT(id) )
+   {
+      jsint slot = JSVAL_TO_INT(id);
+
+printf( "slot == %d\n", slot );
+
+      JS_LOCK_OBJ(cx, obj);
+      jsdouble lastIndex;
+      JSRegExp *re;
+   
+      urlFile_t *fURL = (urlFile_t *)JS_GetInstancePrivate( cx, obj, &jsCurlClass_, NULL );
+      if( fURL ) 
+      {
+         switch (slot) 
+         {
+            case CURLFILE_ISOPEN :  
+               *vp = BOOLEAN_TO_JSVAL( fURL->isOpen() ); 
+               break ;
+            case CURLFILE_SIZE : 
+               *vp = INT_TO_JSVAL( (int)fURL->getSize() ); 
+               break ;
+            case CURLFILE_DATA : 
+               if( fURL->isOpen() )
+               {
+                  JSString *str = JS_NewStringCopyN( cx, (char const *)fURL->getData(), fURL->getSize() );
+                  if( str )
+                     *vp = STRING_TO_JSVAL( str ); 
+               }
+               break ;
+            case CURLFILE_URL : 
+               {
+                  JSString *str = JS_NewStringCopyZ( cx, "url goes here" );
+                  if( str )
+                     *vp = STRING_TO_JSVAL( str ); 
+               }
+               break ;
+            case CURLFILE_HTTPCODE : 
+               *vp = INT_TO_JSVAL( 0x01020304 ); 
+               break ;
+            case CURLFILE_FILETIME : 
+               *vp = INT_TO_JSVAL( 0x02030405 ); 
+               break ;
+            case CURLFILE_MIMETYPE :
+               {
+                  JSString *str = JS_NewStringCopyZ( cx, "text/plain" );
+                  if( str )
+                     *vp = STRING_TO_JSVAL( str ); 
+               }
+               break ;
+         } // switch
+      }
+      else
+         printf( "NULL fURL\n" );
+
+      JS_UNLOCK_OBJ(cx, obj);
+   
+   }
+   else
+      printf( "id not a slot\n" );
+
+   return JS_TRUE;
+}
+
+static JSBool
+jsCurl_setProperty(JSContext *cx, JSObject *obj, jsval id, jsval *vp)
+{
+printf( "setting property for jsCurl object\n" );
+   if( JSVAL_IS_INT(id) )
+   {
+      jsint slot = JSVAL_TO_INT( id );
+      //
+      // all properties are read-only.. No need to set
+      //
+   }
+
+   return JS_TRUE ;
+}
+
+JSClass jsCurlClass_ = {
+  "curlFile",
+   JSCLASS_HAS_PRIVATE,
+   JS_PropertyStub,  JS_PropertyStub,  jsCurl_getProperty,  jsCurl_setProperty,
+   JS_EnumerateStub, JS_ResolveStub,   JS_ConvertStub,      JS_FinalizeStub,
+   JSCLASS_NO_OPTIONAL_MEMBERS
+};
+
+static JSPropertySpec curlFileProperties_[] = {
+  {"isOpen",         CURLFILE_ISOPEN,     JSPROP_ENUMERATE|JSPROP_READONLY|JSPROP_PERMANENT },
+  {"size",           CURLFILE_SIZE,       JSPROP_ENUMERATE|JSPROP_READONLY|JSPROP_PERMANENT },
+  {"data",           CURLFILE_DATA,       JSPROP_ENUMERATE|JSPROP_READONLY|JSPROP_PERMANENT },
+  {"url",            CURLFILE_URL,        JSPROP_ENUMERATE|JSPROP_READONLY|JSPROP_PERMANENT },
+  {"httpCode",       CURLFILE_HTTPCODE,   JSPROP_ENUMERATE|JSPROP_READONLY|JSPROP_PERMANENT },
+  {"fileTime",       CURLFILE_FILETIME,   JSPROP_ENUMERATE|JSPROP_READONLY|JSPROP_PERMANENT },
+  {"mimeType",       CURLFILE_MIMETYPE,   JSPROP_ENUMERATE|JSPROP_READONLY|JSPROP_PERMANENT },
+  {0,0,0}
+};
+
+static JSBool curlFile( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
+{
+printf( "constructing curlFile\n" );
+   if( (argc == 1) && ( 0 != (cx->fp->flags & JSFRAME_CONSTRUCTING) ) )
+   {
+printf( "in curlFile constructor\n" );
+      if( JSVAL_IS_STRING( argv[0] ) )
+      {
+printf( "param is string\n" );
+         obj = js_NewObject( cx, &jsCurlClass_, NULL, NULL );
+
+         if( obj )
+         {
+            char const *url = JS_GetStringBytes( js_ValueToString( cx, argv[0]) );
+printf( "param<%s>\n", url );
+            urlFile_t *fURL = new urlFile_t( (char const *)url );
+            if( fURL )
+            {
+               if( JS_SetPrivate( cx, obj, fURL ) )
+               {
+                  *rval = OBJECT_TO_JSVAL(obj);
+                  return JS_TRUE;
+               }
+               else
+                  delete fURL ;
+            }
+         }
+      }
+   }
+      
+   return JS_FALSE ;
+}
+
+void initJSCurl( JSContext *cx, JSObject *glob )
+{
+   JSObject *rval = JS_InitClass( cx, glob, NULL, &jsCurlClass_,
+                       /* native constructor function and min arg count */
+                       curlFile, 1,
+                       curlFileProperties_, 0,
+                       0, 0 );
+   if( rval )
+      printf( "initialized jsCurlClass successfully\n" );
+   else
+      printf( "error initializing jsCurlClass\n" );
+}
+
+
---
+++ jsCurl.h
@@ -0,0 +1,35 @@
+#ifndef __JSCURL_H__
+#define __JSCURL_H__ "$Id: jsCurl.h,v 1.1 2002-09-29 17:36:23 ericn Exp $"
+
+/*
+ * jsCurl.h
+ *
+ * This header file declares the Javascript wrappers for
+ * curl objects.
+ *                                                                                     static              static
+ *    Object            constructor             methods           properties           methods           properties
+ *    curlFile          curlFile( url )                           isOpen
+ *                                                                size
+ *                                                                data
+ *                                                                url
+ *                                                                httpCode
+ *                                                                fileTime
+ *                                                                mimeType 
+ *
+ * Change History : 
+ *
+ * $Log: jsCurl.h,v $
+ * Revision 1.1  2002-09-29 17:36:23  ericn
+ * -Initial import
+ *
+ *
+ *
+ * Copyright Boundary Devices, Inc. 2002
+ */
+
+#include "js/jsapi.h"
+
+void initJSCurl( JSContext *cx, JSObject *glob );
+
+#endif
+
