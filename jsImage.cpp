6a1c0e46051974fa5967ef9a0b37b78dd4a49f2a
ericn <ericn>
2002-10-31 02:10:58 +0000

    -modified image() constructor to be multi-threaded, use rh object

--- jsImage.cpp
+++ jsImage.cpp
@@ -9,7 +9,10 @@
  * Change History : 
  *
  * $Log: jsImage.cpp,v $
- * Revision 1.5  2002-10-25 13:47:28  ericn
+ * Revision 1.6  2002-10-31 02:10:58  ericn
+ * -modified image() constructor to be multi-threaded, use rh object
+ *
+ * Revision 1.5  2002/10/25 13:47:28  ericn
  * -fixed memory leak, removed debug code
  *
  * Revision 1.4  2002/10/25 04:50:58  ericn
@@ -33,6 +36,12 @@
 #include <libpng12/png.h>
 #include "fbDev.h"
 #include "../boundary1/hexDump.h"
+#include "js/jscntxt.h"
+#include "imgGIF.h"
+#include "imgPNG.h"
+#include "imgJPEG.h"
+#include "curlThread.h"
+#include "jsGlobals.h"
 
 extern "C" {
 #include <jpeglib.h>
@@ -45,9 +54,9 @@ typedef struct {
    size_t      length_ ;
 } pngData_t ;
 
-void pngRead( png_structp png, 
-              png_bytep   data, 
-              png_size_t  len )
+static void pngRead( png_structp png, 
+                     png_bytep   data, 
+                     png_size_t  len )
 {
    pngData_t *pData = (pngData_t *)png->io_ptr ;
    if( len <= pData->length_ )
@@ -61,7 +70,7 @@ void pngRead( png_structp png,
 }
 
 static JSBool
-imagePNG(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
+jsImagePNG(JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval)
 {
    //
    // need at least data, plus optional x and y
@@ -260,7 +269,7 @@ static void jpg_term_source( j_decompress_ptr cinfo )
 }
 
 static JSBool
-imageJPEG( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
+jsImageJPEG( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
 {
    //
    // need at least data, plus optional x and y
@@ -521,7 +530,7 @@ static void displayGIF( fbDevice_t           &fb,
 }
 
 static JSBool
-imageGIF( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
+jsImageGIF( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
 {
    //
    // need at least data, plus optional x and y
@@ -626,15 +635,251 @@ imageGIF( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
 }
 
 static JSFunctionSpec image_functions[] = {
-    {"imagePNG",         imagePNG,        1 },
-    {"imageJPEG",        imageJPEG,       1 },
-    {"imageGIF",         imageGIF,        1 },
+    {"imagePNG",         jsImagePNG,        1 },
+    {"imageJPEG",        jsImageJPEG,       1 },
+    {"imageGIF",         jsImageGIF,        1 },
     {0}
 };
 
+static JSBool
+jsImageDraw( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
+{
+   //
+   // need x and y
+   //
+   if( 2 == argc )
+   {
+      int xPos = JSVAL_TO_INT( argv[0] );
+      int yPos = JSVAL_TO_INT( argv[1] );
+
+//      JS_ReportError( cx, "x:%d, y:%d", xPos, yPos );
+
+      jsval widthVal, heightVal, dataVal ;
+
+      if( JS_GetProperty( cx, obj, "width", &widthVal )
+          &&
+          JS_GetProperty( cx, obj, "height", &heightVal )
+          &&
+          JS_GetProperty( cx, obj, "pixBuf", &dataVal ) )
+      {
+         int width  = JSVAL_TO_INT( widthVal ); 
+         int height = JSVAL_TO_INT( heightVal );
+         JSString *pixStr = JSVAL_TO_STRING( dataVal );
+         unsigned short const *const pixMap = (unsigned short *)JS_GetStringBytes( pixStr );
+
+         fbDevice_t &fb = getFB();
+         if( ( 0 < width ) && ( 0 < height ) && ( xPos < fb.getWidth() ) && ( yPos < fb.getHeight() ) )
+         {
+            unsigned const left = xPos ;
+
+            for( unsigned y = 0 ; y < height ; y++, yPos++ )
+            {
+               if( yPos < fb.getHeight() )
+               {
+                  unsigned short *pix = fb.getRow( yPos ) + left ;
+                  xPos = left ;
+                  for( unsigned x = 0 ; x < width ; x++, xPos++ )
+                  {
+                     if( xPos < fb.getWidth() )
+                     {
+                        *pix++ = pixMap[y*width+x];
+                     }
+                     else
+                        break;
+                  }
+               }
+               else
+                  break;
+            }
+         }
+//         JS_ReportError( cx, "w:%d, h:%d", width, height );
+      }
+
+      *rval = JSVAL_TRUE ;
+      return JS_TRUE ;
+
+   } // need at least two params
+   else
+   {
+      JS_ReportError( cx, "Usage: image().draw( x, y )" );
+      JS_ReportError( cx, "#args == %d", argc );
+   }
+
+   return JS_FALSE ;
+}
+
+static JSFunctionSpec imageMethods_[] = {
+    {"draw",         jsImageDraw,           3 },
+    {0}
+};
+
+enum jsImage_tinyId {
+   IMAGE_WIDTH, 
+   IMAGE_HEIGHT, 
+   IMAGE_PIXBUF,
+};
+
+extern JSClass jsImageClass_ ;
+
+JSClass jsImageClass_ = {
+  "image",
+   JSCLASS_HAS_PRIVATE,
+   JS_PropertyStub,  JS_PropertyStub,  JS_PropertyStub,     JS_PropertyStub,
+   JS_EnumerateStub, JS_ResolveStub,   JS_ConvertStub,      JS_FinalizeStub,
+   JSCLASS_NO_OPTIONAL_MEMBERS
+};
+
+static JSPropertySpec imageProperties_[] = {
+  {"width",         IMAGE_WIDTH,     JSPROP_ENUMERATE|JSPROP_READONLY|JSPROP_PERMANENT },
+  {"height",        IMAGE_HEIGHT,    JSPROP_ENUMERATE|JSPROP_READONLY|JSPROP_PERMANENT },
+  {"pixBuf",        IMAGE_PIXBUF,    JSPROP_ENUMERATE|JSPROP_READONLY|JSPROP_PERMANENT },
+  {0,0,0}
+};
+
+static void imageOnComplete( jsCurlRequest_t &req, curlFile_t const &f )
+{
+   char const *cMime = f.getMimeType();
+   char const *cData = (char const *)f.getData();
+   unsigned    size  = f.getSize();
+
+   bool            worked = false ;
+   void const     *pixMap ;
+   unsigned short  width ;
+   unsigned short  height ;
+   std::string     sError ;
+   if( ( 0 == strcmp( cMime, "image/png" ) )
+       ||
+       ( 0 == strcmp( cMime, "image/x-png" ) ) )
+   {
+      worked = imagePNG( cData, size, pixMap, width, height );
+      if( !worked )
+         sError = "error converting PNG" ;
+   }
+   else if( 0 == strcmp( cMime, "image/jpeg" ) )
+   {
+      worked = imageJPEG( cData, size, pixMap, width, height );
+      if( !worked )
+         sError = "error converting PNG" ;
+   }
+   else if( 0 == strcmp( cMime, "image/gif" ) )
+   {
+      worked = imageGIF( cData, size, pixMap, width, height );
+      if( !worked )
+         sError = "error converting PNG" ;
+   }
+   else
+   {
+      sError = "unknown mime type: " ;
+      sError += cMime ;
+   }
+
+   if( worked )
+   {
+      JS_DefineProperty( req.cx_, req.lhObj_, "width",
+                         INT_TO_JSVAL( width ),
+                         0, 0, 
+                         JSPROP_ENUMERATE
+                         |JSPROP_PERMANENT
+                         |JSPROP_READONLY );
+      JS_DefineProperty( req.cx_, req.lhObj_, "height",
+                         INT_TO_JSVAL( height ),
+                         0, 0, 
+                         JSPROP_ENUMERATE
+                         |JSPROP_PERMANENT
+                         |JSPROP_READONLY );
+      JSString *sPix = JS_NewStringCopyN( req.cx_, (char const *)pixMap, width*height*sizeof(unsigned short) );
+      JS_DefineProperty( req.cx_, req.lhObj_, "pixBuf",
+                         STRING_TO_JSVAL( sPix ),
+                         0, 0, 
+                         JSPROP_ENUMERATE
+                         |JSPROP_PERMANENT
+                         |JSPROP_READONLY );
+      jsCurlOnComplete( req, f );
+   }
+   else
+   {   
+      JSString *errorStr = JS_NewStringCopyN( req.cx_, sError.c_str(), sError.size() );
+      JS_DefineProperty( req.cx_, 
+                         req.lhObj_, 
+                         "loadErrorMsg",
+                         STRING_TO_JSVAL( errorStr ),
+                         0, 0, 
+                         JSPROP_ENUMERATE
+                         |JSPROP_PERMANENT
+                         |JSPROP_READONLY );
+      jsCurlOnError( req, f );
+   }
+}
+
+static JSBool image( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
+{
+   *rval = JSVAL_FALSE ;
+   if( ( 1 == argc ) 
+       && 
+       ( 0 != (cx->fp->flags & JSFRAME_CONSTRUCTING) ) 
+       &&
+       JSVAL_IS_OBJECT( argv[0] ) )
+   {
+      JSObject *thisObj = js_NewObject( cx, &jsImageClass_, NULL, NULL );
+
+      if( thisObj )
+      {
+         *rval = OBJECT_TO_JSVAL( thisObj ); // root
+         JS_DefineProperty( cx, thisObj, "isLoaded",
+                            JSVAL_FALSE,
+                            0, 0, 
+                            JSPROP_ENUMERATE
+                            |JSPROP_PERMANENT
+                            |JSPROP_READONLY );
+         JS_DefineProperty( cx, thisObj, "initializer",
+                            argv[0],
+                            0, 0, 
+                            JSPROP_ENUMERATE
+                            |JSPROP_PERMANENT
+                            |JSPROP_READONLY );
+         JSObject *const rhObj = JSVAL_TO_OBJECT( argv[0] );
+         
+         jsCurlRequest_t request ;
+         request.onComplete = imageOnComplete ;
+         request.onError    = jsCurlOnError ;
+         request.lhObj_ = thisObj ;
+         request.rhObj_ = rhObj ;
+         request.cx_    = cx ;
+         request.mutex_ = &execMutex_ ;
+
+         if( queueCurlRequest( request ) )
+         {
+            return JS_TRUE ;
+         }
+         else
+         {
+            JS_ReportError( cx, "Error queueing curlRequest" );
+         }
+      }
+      else
+         JS_ReportError( cx, "Error allocating curlFile" );
+   }
+   else
+      JS_ReportError( cx, "Usage : new image( { url:\"something\" } );" );
+      
+   return JS_TRUE ;
+
+}
 
 bool initJSImage( JSContext *cx, JSObject *glob )
 {
-   return JS_DefineFunctions( cx, glob, image_functions);
+   JSObject *rval = JS_InitClass( cx, glob, NULL, &jsImageClass_,
+                                  image, 1,
+                                  imageProperties_, 
+                                  imageMethods_,
+                                  0, 0 );
+   if( rval )
+   {
+      return JS_DefineFunctions( cx, glob, image_functions);
+
+   }
+   else
+      return false ;
+
 }
 
