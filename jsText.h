393a7a307823f7d333657af6f44adff04a0a4037
ericn <ericn>
2002-11-02 04:10:59 +0000

    -added font comments

--- jsText.h
+++ jsText.h
@@ -1,11 +1,11 @@
 #ifndef __JSTEXT_H__
-#define __JSTEXT_H__ "$Id: jsText.h,v 1.2 2002-10-20 16:29:44 ericn Exp $"
+#define __JSTEXT_H__ "$Id: jsText.h,v 1.3 2002-11-02 04:10:59 ericn Exp $"
 
 /*
  * jsText.h
  *
  * This header file declares the initialization routine
- * for the text output routines:
+ * for the text output routines and freeType classes:
  *
  *    jsTextAt( fontData( string ), 
  *              pointSize, 
@@ -14,10 +14,19 @@
  *              [fgColor=(RGB)( > 0xFFFFFF == default white) ], 
  *              [bgColor=(RGB)( > 0xFFFFFF == default black) ] );
  *
+ *    object font( { url:"something", 
+ *                   [useCache:true], 
+ *                   [onLoad="doSomething();"], 
+ *                   [onLoadError="somethingElse();"] } );
+ *
+ *
  * Change History : 
  *
  * $Log: jsText.h,v $
- * Revision 1.2  2002-10-20 16:29:44  ericn
+ * Revision 1.3  2002-11-02 04:10:59  ericn
+ * -added font comments
+ *
+ * Revision 1.2  2002/10/20 16:29:44  ericn
  * -fixed comment
  *
  * Revision 1.1  2002/10/18 01:18:25  ericn
