da46ae271bd93c889440f18a6a517a33b5b7685e
ericn <ericn>
2002-11-07 02:14:34 +0000

    -removed bare functions

--- jsMP3.cpp
+++ jsMP3.cpp
@@ -9,7 +9,10 @@
  * Change History : 
  *
  * $Log: jsMP3.cpp,v $
- * Revision 1.8  2002-11-05 15:16:07  ericn
+ * Revision 1.9  2002-11-07 02:14:34  ericn
+ * -removed bare functions
+ *
+ * Revision 1.8  2002/11/05 15:16:07  ericn
  * -more implementation of mp3File class and function
  *
  * Revision 1.7  2002/11/05 05:43:05  ericn
@@ -41,190 +44,80 @@
 #include "jsMP3.h"
 #include "relativeURL.h"
 #include "curlCache.h"
-#include "childProcess.h"
 #include <unistd.h>
 #include <list>
 #include "js/jscntxt.h"
 #include "curlThread.h"
 #include "mad.h"
 #include "madHeaders.h"
-
-typedef struct playListEntry_t {
-   std::string url_ ;
-   std::string onComplete_ ;
-   std::string onCancel_ ;
-   std::string onError_ ;
-};
-
-typedef std::list<playListEntry_t> playList_t ;
-
-static playList_t playList_ ;
-
-class mp3Process_t : public childProcess_t {
-public:
-   virtual void    died( void );
-   playListEntry_t details_ ;
-};
-
-void mp3Process_t :: died( void )
-{
-//   printf( "mp3 process died\n" );
-   pid_ = -1 ;
-   printf( "finished playback of %s\n", details_.url_.c_str() );
-
-   if( !playList_.empty() )
-   {
-      details_ = playList_.front();
-      playList_.pop_front();
-      std::string const url = details_.url_ ;
-//      printf( "starting %s\n", url.c_str() );
-      char *args[3] = {
-         "./mp3Play",
-         (char *)url.c_str(),
-         0
-      };
-
-      run( args[0], args, __environ );
-   }
-}
-
-static mp3Process_t    curProcess_ ;
+#include "audioQueue.h"
 
 static JSBool
 jsMP3Play( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
 {
-   if( ( 1 <= argc )
+   jsval                dataVal ;
+   JSString            *dataStr ;
+   unsigned char const *data ;
+   if( JS_GetProperty( cx, obj, "data", &dataVal ) 
        &&
-       JSVAL_IS_STRING( argv[0] ) )
+       JSVAL_IS_STRING( dataVal ) 
+       &&
+       ( 0 != ( dataStr = JSVAL_TO_STRING( dataVal ) ) ) 
+       &&
+       ( 0 != ( data = (unsigned char *)JS_GetStringBytes( dataStr ) ) ) )
    {
-      curlCache_t &cache = getCurlCache();
-      char *cURL = JS_GetStringBytes( JS_ValueToString( cx, argv[0] ) );
-      curlFile_t f( cache.get( cURL, true ) );
-      if( f.isOpen() )
+      unsigned const length = JS_GetStringLength( dataStr );
+
+      std::string onComplete ;
+      std::string onCancel ;
+      
+      JSObject *rhObj ;
+      if( ( 1 <= argc )
+          &&
+          JSVAL_IS_OBJECT( argv[0] ) 
+          &&
+          ( 0 != ( rhObj = JSVAL_TO_OBJECT( argv[0] ) ) ) )
       {
-         std::string onComplete ;
-         std::string onCancel ;
-         std::string onError ;
-
-         if( ( 2 <= argc ) && JSVAL_IS_STRING( argv[1] ) )
-         {
-            onComplete = JS_GetStringBytes( JS_ValueToString( cx, argv[1] ) );
-            if( ( 3 <= argc ) && JSVAL_IS_STRING( argv[2] ) )
-            {
-               onCancel = JS_GetStringBytes( JS_ValueToString( cx, argv[2] ) );
-               if( ( 4 <= argc ) && JSVAL_IS_STRING( argv[3] ) )
-               {
-                  onError = JS_GetStringBytes( JS_ValueToString( cx, argv[3] ) );
-               }
-            }
-         }
-//         printf( "mime type %s\n", f.getMimeType() );
-         childProcessLock_t lock ;
-
-         std::string absolute ;
-         absoluteURL( cURL, absolute );
+         jsval     val ;
+         JSString *sHandler ;
          
-         playListEntry_t next ;
-         next.url_        = absolute ;
-         next.onComplete_ = onComplete ;
-         next.onCancel_   = onCancel ;
-         next.onError_    = onError ;
-
-         if( !curProcess_.isRunning() )
+         if( JS_GetProperty( cx, rhObj, "onComplete", &val ) 
+             &&
+             JSVAL_IS_STRING( val ) 
+             &&
+             ( 0 != ( sHandler = JSVAL_TO_STRING( val ) ) ) )
          {
-            curProcess_.details_ = next ;
-
-            char *args[3] = {
-               "./mp3Play",
-               (char *)absolute.c_str(),
-               0
-            };
-
-            if( curProcess_.run( args[0], args, __environ ) )
-            {
-//               printf( "playing %s\n", cURL );
-            }
-            else
-            {
-               printf( "Error %m running ./mp3Play %s\n", cURL );
-            }
-         }
-         else
+            onComplete = JS_GetStringBytes( sHandler );
+         } // have onComplete handler
+         
+         if( JS_GetProperty( cx, rhObj, "onCancel", &val ) 
+             &&
+             JSVAL_IS_STRING( val ) 
+             &&
+             ( 0 != ( sHandler = JSVAL_TO_STRING( val ) ) ) )
          {
-//            printf( "queueing %s\n", absolute.c_str() );
-            playList_.push_back( next );
-         }
-
-         *rval = JSVAL_TRUE ;
+            onCancel = JS_GetStringBytes( sHandler );
+         } // have onComplete handler
+      } // have right-hand object
+      
+      if( getAudioQueue().insert( obj, data, length, onComplete, onCancel ) )
+      {
+         JS_ReportError( cx, "queued MP3 for playback" );
       }
       else
       {
-         fprintf( stderr, "Error retrieving %s\n", cURL );
-         *rval = JSVAL_FALSE ;
+         JS_ReportError( cx, "Error queueing MP3 for playback" );
       }
    }
    else
-   {
-      printf( "usage : mp3Play( url )\n" );
-      *rval = JSVAL_FALSE ;
-   }
-   
-   return JS_TRUE ;
-}
+      JS_ReportError( cx, "Error retrieving MP3 data" );
 
-static JSBool
-jsMP3Wait( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
-{
-   while( 1 )
-   {
-      bool somethingQueued_ ;
-      {
-         childProcessLock_t lock ;
-         somethingQueued_ = ( curProcess_.isRunning() || !playList_.empty() );
-      }
-      if( somethingQueued_ )
-         pause();
-      else
-         break;
-   }
-   
    *rval = JSVAL_TRUE ;
    return JS_TRUE ;
 }
 
-static JSBool
-jsMP3Skip( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
-{
-   childProcessLock_t lock ;
-   if( curProcess_.isRunning() )
-      kill( curProcess_.pid_, 9 );
-   
-   *rval = JSVAL_TRUE ;
-   return JS_TRUE ;
-}
-
-static JSBool
-jsMP3Count( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
-{
-   int numQueued = 0 ;
-
-   {
-      childProcessLock_t lock ;
-      if( curProcess_.isRunning() )
-         numQueued = 1 ;
-   
-      numQueued += playList_.size();
-   }
-
-   *rval = INT_TO_JSVAL( numQueued );
-   return JS_TRUE ;
-}
-
-static JSFunctionSpec mp3_functions[] = {
-    {"mp3Play",           jsMP3Play,        1 },
-    {"mp3Wait",           jsMP3Wait,        1 },
-    {"mp3Skip",           jsMP3Skip,        1 },
-    {"mp3Count",          jsMP3Count,       1 },
+static JSFunctionSpec mp3Methods_[] = {
+    {"play",         jsMP3Play,           0 },
     {0}
 };
 
@@ -382,11 +275,11 @@ bool initJSMP3( JSContext *cx, JSObject *glob )
    JSObject *rval = JS_InitClass( cx, glob, NULL, &jsMp3Class_,
                                   mp3File, 1,
                                   mp3FileProperties_, 
-                                  0,
+                                  mp3Methods_,
                                   0, 0 );
    if( rval )
    {
-      return JS_DefineFunctions( cx, glob, mp3_functions);
+      return true ;
    }
    else
       return false ;
