7aa5cd8071c4e8a3ec33857841ac9953c242a85a
ericn <ericn>
2002-11-17 16:09:22 +0000

    -fixed non-rooted JSScript bug

--- codeQueue.cpp
+++ codeQueue.cpp
@@ -7,7 +7,10 @@
  * Change History : 
  *
  * $Log: codeQueue.cpp,v $
- * Revision 1.2  2002-10-31 02:09:38  ericn
+ * Revision 1.3  2002-11-17 16:09:22  ericn
+ * -fixed non-rooted JSScript bug
+ *
+ * Revision 1.2  2002/10/31 02:09:38  ericn
  * -added scope to code queue
  *
  * Revision 1.1  2002/10/27 17:42:08  ericn
@@ -22,8 +25,9 @@
 #include "mtQueue.h"
 
 typedef struct scriptAndScope_t {
-   JSObject *scope_ ;
-   JSScript *script_ ;
+   JSObject    *scope_ ;
+   std::string  script_ ;
+   std::string  source_ ;
 };
 
 typedef mtQueue_t<scriptAndScope_t> codeList_t ;
@@ -40,23 +44,16 @@ bool queueSource( JSObject          *scope,
                   std::string const &sourceCode,
                   char const        *sourceFile )
 {
-   JSScript *script= JS_CompileScript( context_, scope, 
-                                       sourceCode.c_str(), 
-                                       sourceCode.size(), 
-                                       sourceFile, 1 );
-   if( script )
-   {
-      scriptAndScope_t item ;
-      item.script_ = script ;
-      item.scope_  = scope ;
+   scriptAndScope_t item ;
+   item.script_ = sourceCode ;
+   item.scope_  = scope ;
+   item.source_ = sourceFile ;
 
-      if( codeList_.push( item ) )
-         return true ;
-      else
-         fprintf( stderr, "Error queueing code\n" );
-   }
+   bool const worked = codeList_.push( item );
+   if( worked )
+      return true ;
    else
-      fprintf( stderr, "Error compiling code %s\n", sourceCode.c_str() );
+      fprintf( stderr, "Error queueing code\n" );
 
    return false ;
 }
@@ -78,9 +75,18 @@ bool dequeueByteCode( JSScript    *&script,
                        : codeList_.pull( item, milliseconds );
    if( result )
    {
-      script = item.script_ ;
-      scope  = item.scope_ ;
-      return true ;
+      JSScript *scr = JS_CompileScript( context_, item.scope_, 
+                                        item.script_.c_str(), 
+                                        item.script_.size(), 
+                                        item.source_.c_str(), 1 );
+      if( scr )
+      {
+         script = scr ;
+         scope  = item.scope_ ;
+         return true ;
+      }
+      else
+         fprintf( stderr, "Error compiling code %s\n", item.script_.c_str() );
    }
    else
       return false ;
