5f3656e2751c06bf5c23ea965bae3fbf4792ee57
ericn <ericn>
2002-11-23 16:05:24 +0000

    -added alpha channel

--- imgPNG.cpp
+++ imgPNG.cpp
@@ -8,7 +8,10 @@
  * Change History : 
  *
  * $Log: imgPNG.cpp,v $
- * Revision 1.3  2002-11-20 00:39:22  ericn
+ * Revision 1.4  2002-11-23 16:05:26  ericn
+ * -added alpha channel
+ *
+ * Revision 1.3  2002/11/20 00:39:22  ericn
  * -modified to catch errors
  *
  * Revision 1.2  2002/11/11 04:29:14  ericn
@@ -47,13 +50,38 @@ static void pngRead( png_structp png,
       png_error( png, "short read");
 }
 
+#ifdef __STANDALONE__
+
+static void dumpPNGInfo( png_infop pi )
+{
+   printf( "width %u\n", pi->width );       /* width of image in pixels (from IHDR) */
+   printf( "height %u\n", pi->height );      /* height of image in pixels (from IHDR) */
+   printf( "valid %u\n", pi->valid );       /* valid chunk data (see PNG_INFO_ below) */
+   printf( "rowbytes %u\n", pi->rowbytes );    /* bytes needed to hold an untransformed row */
+   printf( "palette %u\n", pi->palette );      /* array of color values (valid & PNG_INFO_PLTE) */
+   printf( "num_palette %u\n", pi->num_palette ); /* number of color entries in "palette" (PLTE) */
+   printf( "num_trans %u\n", pi->num_trans );   /* number of transparent palette color (tRNS) */
+   printf( "bit_depth %u\n", pi->bit_depth );      /* 1, 2, 4, 8, or 16 bits/channel (from IHDR) */
+   printf( "color_type %u\n", pi->color_type );     /* see PNG_COLOR_TYPE_ below (from IHDR) */
+   printf( "compression_type %u\n", pi->compression_type ); /* must be PNG_COMPRESSION_TYPE_BASE (IHDR) */
+   printf( "filter_type %u\n", pi->filter_type );    /* must be PNG_FILTER_TYPE_BASE (from IHDR) */
+   printf( "interlace_type %u\n", pi->interlace_type ); /* One of PNG_INTERLACE_NONE, PNG_INTERLACE_ADAM7 */
+
+   printf( "channels %u\n", pi->channels );       /* number of data channels per pixel (1, 2, 3, 4) */
+   printf( "pixel_depth %u\n", pi->pixel_depth );    /* number of bits per pixel */
+   printf( "spare_byte %u\n", pi->spare_byte );     /* to align the data, and for future use */
+
+}
+#endif
+
 bool imagePNG( void const    *inData,     // input
                unsigned long  inSize,     // input
                void const    *&pixData,   // output
                unsigned short &width,     // output
-               unsigned short &height )   // output
+               unsigned short &height,    // output
+               void const    *&alpha )    // output : 0 if none, delete [] when done
 {
-   pixData = 0 ; width = height = 0 ;
+   pixData = alpha = 0 ; width = height = 0 ;
 
    png_bytep pngData = (png_bytep)inData ;
    unsigned const pngLength = inSize ;
@@ -80,7 +108,9 @@ bool imagePNG( void const    *inData,     // input
                         png_set_read_fn( png_ptr, &pngFile, pngRead );
 
                         png_read_info(png_ptr, info_ptr);
-
+#ifdef __STANDALONE__
+dumpPNGInfo( info_ptr );
+#endif
                         int interlace_type, compression_type, filter_type;
                         png_uint_32 lWidth, lHeight;
                         int bit_depth, color_type;
@@ -108,17 +138,24 @@ bool imagePNG( void const    *inData,     // input
 
                         fbDevice_t &fb = getFB();
 
+                        unsigned char *const alphaChannel = ( color_type & PNG_COLOR_MASK_ALPHA )
+                                                            ? new unsigned char [ width*height ]
+                                                            : 0 ;
+
                         unsigned short *const pixMap = new unsigned short [ width*height ];
                         for( unsigned row = 0 ; row < height ; row++ )
                         {
                            unsigned char const *imgRow = row_pointers[row];
-                           for( unsigned column = 0 ; column < width ; column++, imgRow += 3 )
+                           for( unsigned column = 0 ; column < width ; column++, imgRow += info_ptr->channels )
                            {
                               pixMap[row*width+column] = fb.get16( imgRow[0], imgRow[1], imgRow[2] );
+                              if( alphaChannel )
+                                 alphaChannel[row*width+column] = imgRow[3];
                            }
                         }
 
                         pixData = pixMap ;
+                        alpha   = alphaChannel ;
 
                         for( unsigned row = 0; row < lHeight ; row++ )
                            png_free( png_ptr, row_pointers[row] );
@@ -146,3 +183,54 @@ bool imagePNG( void const    *inData,     // input
 
 }
 
+
+
+#ifdef __STANDALONE__
+
+#include "hexDump.h"
+#include "memFile.h"
+
+int main( int argc, char const * const argv[] )
+{
+   if( 2 <= argc )
+   {
+      memFile_t fIn( argv[1] );
+      if( fIn.worked() )
+      {
+         printf( "%lu bytes read at address %p\n", fIn.getLength(), fIn.getData() );
+         void const *pixData ;
+         void const *alpha ;
+         unsigned short width ;
+         unsigned short height ;
+         if( imagePNG( fIn.getData(), fIn.getLength(),
+                       pixData,
+                       width,
+                       height,
+                       alpha ) )
+         {
+            printf( "image: %u x %u pixels\n", width, height );
+            unsigned long size = (unsigned long)width * height * 2 ;
+//            hexDumper_t dump( pixData, size );
+//            while( dump.nextLine() )
+//               printf( "%s\n", dump.getLine() );
+            if( alpha )
+            {
+               printf( "has alpha channel\n" );
+               hexDumper_t dumpAlpha( alpha, width*height );
+               while( dumpAlpha.nextLine() )
+                  printf( "%s\n", dumpAlpha.getLine() );
+            }
+         }
+         else
+            fprintf( stderr, "Error converting image\n" );
+      }
+      else
+         fprintf( stderr, "Error %s opening %s\n", fIn.getError(), argv[1] );
+   }
+   else
+      fprintf( stderr, "Usage : imgPNG fileName\n" );
+
+   return 0 ;
+}
+
+#endif
--- imgPNG.h
+++ imgPNG.h
@@ -1,18 +1,21 @@
 #ifndef __IMGPNG_H__
-#define __IMGPNG_H__ "$Id: imgPNG.h,v 1.2 2002-11-20 00:38:28 ericn Exp $"
+#define __IMGPNG_H__ "$Id: imgPNG.h,v 1.3 2002-11-23 16:05:24 ericn Exp $"
 
 /*
  * imgPNG.h
  *
  * This header file declares the imagePNG() routine,
  * which tries to convert a hunk of memory into a pixmap
- * by translating PNG.
+ * and optional alpha channel by translating PNG.
  *
  *
  * Change History : 
  *
  * $Log: imgPNG.h,v $
- * Revision 1.2  2002-11-20 00:38:28  ericn
+ * Revision 1.3  2002-11-23 16:05:24  ericn
+ * -added alpha channel
+ *
+ * Revision 1.2  2002/11/20 00:38:28  ericn
  * -added comment
  *
  * Revision 1.1  2002/10/31 02:13:08  ericn
@@ -27,7 +30,8 @@ bool imagePNG( void const    *inData,     // input
                unsigned long  inSize,     // input
                void const    *&pixData,   // output : delete [] when done
                unsigned short &width,     // output
-               unsigned short &height );  // output
+               unsigned short &height,    // output
+               void const    *&alpha );   // output : 0 if none, delete [] when done
 
 
 
