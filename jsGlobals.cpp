d9cfe4a784798651e7d9adbb65514b0488e3d300
ericn <ericn>
2002-12-02 15:08:55 +0000

    -added mutex name

--- jsGlobals.cpp
+++ jsGlobals.cpp
@@ -8,7 +8,10 @@
  * Change History : 
  *
  * $Log: jsGlobals.cpp,v $
- * Revision 1.1  2002-10-31 02:13:08  ericn
+ * Revision 1.2  2002-12-02 15:08:55  ericn
+ * -added mutex name
+ *
+ * Revision 1.1  2002/10/31 02:13:08  ericn
  * -Initial import
  *
  *
@@ -18,7 +21,7 @@
 
 #include "jsGlobals.h"
 
-mutex_t execMutex_ ;
+mutex_t execMutex_( "jsExecMutex" );
 JSContext *execContext_ = 0 ;
 
 
