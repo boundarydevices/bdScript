2fa48ce32e787dd03809ab25ef5c39ae7cc5d5b5
ericn <ericn>
2003-03-13 14:46:51 +0000

    -added method dissolve()

--- jsImage.cpp
+++ jsImage.cpp
@@ -9,7 +9,10 @@
  * Change History : 
  *
  * $Log: jsImage.cpp,v $
- * Revision 1.21  2002-12-15 20:01:30  ericn
+ * Revision 1.22  2003-03-13 14:46:51  ericn
+ * -added method dissolve()
+ *
+ * Revision 1.21  2002/12/15 20:01:30  ericn
  * -modified to use JS_NewObject instead of js_NewObject
  *
  * Revision 1.20  2002/12/03 13:36:13  ericn
@@ -153,8 +156,160 @@ jsImageDraw( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval 
    return JS_FALSE ;
 }
 
+//
+// number of bits needed to represent n
+//
+static unsigned char bitWidth( unsigned long n )
+{
+   unsigned char width = 0 ; 
+   while( n > 0 )
+   {
+      width++ ;
+      n >>= 1 ;
+   }
+   return width ;
+}
+
+static unsigned long const randMasks[ 33 ] = {
+          0x0, 
+          0x0, 
+         0x03, 
+         0x06, 
+         0x0C, 
+         0x14, 
+         0x30, 
+         0x60, 
+         0xB8, 
+       0x0110, 
+       0x0240, 
+       0x0500, 
+       0x0CA0, 
+       0x1B00, 
+       0x3500, 
+       0x6000, 
+       0xB400, 
+   0x00012000, 
+   0x00020400, 
+   0x00072000, 
+   0x00090000, 
+   0x00140000, 
+   0x00300000, 
+   0x00400000, 
+   0x00D80000, 
+   0x01200000, 
+   0x03880000, 
+   0x07200000, 
+   0x09000000, 
+   0x14000000, 
+   0x32800000, 
+   0x48000000, 
+   0xA3000000
+};
+
+
+JSBool
+jsImageDissolve( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
+{
+   //
+   // need x and y
+   //
+   if( 2 == argc )
+   {
+      int const xPos = JSVAL_TO_INT( argv[0] );
+      int const yPos = JSVAL_TO_INT( argv[1] );
+
+//      JS_ReportError( cx, "x:%d, y:%d", xPos, yPos );
+
+      jsval widthVal, heightVal, dataVal ;
+
+      if( JS_GetProperty( cx, obj, "width", &widthVal )
+          &&
+          JS_GetProperty( cx, obj, "height", &heightVal )
+          &&
+          JS_GetProperty( cx, obj, "pixBuf", &dataVal )
+          &&
+          JSVAL_IS_STRING( dataVal ) )
+      {
+         int const width  = JSVAL_TO_INT( widthVal ); 
+         int const height = JSVAL_TO_INT( heightVal );
+         JSString *pixStr = JSVAL_TO_STRING( dataVal );
+         unsigned short const *const pixMap = (unsigned short *)JS_GetStringBytes( pixStr );
+         if( JS_GetStringLength( pixStr ) == width * height * sizeof( pixMap[0] ) )
+         {   
+            jsval     alphaVal ;
+            JSString *sAlpha ;
+            unsigned char const *alpha ;
+            if( JS_GetProperty( cx, obj, "alpha", &alphaVal ) 
+                &&
+                JSVAL_IS_STRING( alphaVal )
+                &&
+                ( 0 != ( sAlpha = JSVAL_TO_STRING( alphaVal ) ) ) )
+            {
+               alpha = (unsigned char const *)JS_GetStringBytes( sAlpha );
+            } // have alpha channel... use it
+            else
+               alpha = 0 ;
+            
+            //
+            // now dissolve. Taken from Graphics Gems, dissolve2 algorithm
+            //
+            unsigned short const rwidth = bitWidth(height);
+            unsigned short const cwidth = bitWidth(width);
+            unsigned char regWidth = rwidth+cwidth ;
+            if( regWidth && ( regWidth <= 32 ) )
+            {
+               fbDevice_t &fb = getFB();
+               unsigned long const mask = randMasks[regWidth];
+               unsigned char const rowShift = cwidth ;
+               unsigned long const colMask = (1UL << cwidth)-1 ;
+               unsigned long element = 1 ;
+               do {
+                  unsigned long row = element >> rowShift ;
+                  unsigned long column = element & colMask ;
+                  unsigned long const targetX = xPos + column ;
+                  unsigned long const targetY = yPos + row ;
+                  if( ( row < height ) && ( column < width ) 
+                      &&
+                      ( targetX < fb.getWidth() ) && ( targetY < fb.getHeight() ) )
+                  {
+                     fb.getPixel( targetX, targetY ) = pixMap[row*width+column];
+                  } // copy pixel
+
+                  if( element & 1 )
+                     element = ( element >> 1 ) ^ mask ;
+                  else
+                     element = element >> 1 ;
+
+               } while( 1 != element );
+               
+               fb.getPixel( xPos, yPos ) = *pixMap ;
+
+            }
+         }
+         else
+            JS_ReportError( cx, "Invalid pixMap\n" );
+
+//         JS_ReportError( cx, "w:%d, h:%d", width, height );
+      }
+      else
+         JS_ReportError( cx, "Object not initialized, can't draw\n" );
+
+      *rval = JSVAL_TRUE ;
+      return JS_TRUE ;
+
+   } // need at least two params
+   else
+   {
+      JS_ReportError( cx, "Usage: image().draw( x, y )" );
+      JS_ReportError( cx, "#args == %d", argc );
+   }
+
+   return JS_FALSE ;
+}
+
 static JSFunctionSpec imageMethods_[] = {
     {"draw",         jsImageDraw,           3 },
+    {"dissolve",     jsImageDissolve,       3 },
     {0}
 };
 
