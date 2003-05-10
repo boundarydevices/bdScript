16d8f2d0b59b9832efe856331767c5c232641648
ericn <ericn>
2003-05-10 19:16:43 +0000

    -added cut command, modified to keep printer fd

--- jsCBM.cpp
+++ jsCBM.cpp
@@ -8,7 +8,10 @@
  * Change History : 
  *
  * $Log: jsCBM.cpp,v $
- * Revision 1.2  2003-05-10 03:18:54  ericn
+ * Revision 1.3  2003-05-10 19:16:43  ericn
+ * -added cut command, modified to keep printer fd
+ *
+ * Revision 1.2  2003/05/10 03:18:54  ericn
  * -removed redundant test, changed CBM print command
  *
  * Revision 1.1  2003/05/09 04:28:12  ericn
@@ -51,6 +54,8 @@ static JSPropertySpec cbmProperties_[] = {
   {0,0,0}
 };
 
+static int printerFd_ = -1 ;
+
 static JSBool
 jsCBMPrint( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
 {
@@ -89,17 +94,15 @@ fprintf( stderr, "--> Printing something\n" );
          if( JS_GetStringLength( sPixMap ) == bmWidth*bmHeight )
          {
             unsigned char const * const alphaData = (unsigned char const *)JS_GetStringBytes( sPixMap );
-            int const fd = open( "/dev/usb/lp0", O_WRONLY );
-            if( 0 <= fd )
+
+            if( 0 <= printerFd_ )
             {
-               printf( "printer port opened\n" );
-         
                unsigned const width = bmWidth ;
                unsigned const wBits = ((width+7)/8)*8 ;
                unsigned const height = bmHeight ;
                unsigned const hBytes = (height+7)/8 ;
                unsigned const headerLen = 4 ;
-               unsigned const trailerLen = 9 ;
+               unsigned const trailerLen = 3 ;
                char * const outBuf = new char [ headerLen+trailerLen + wBits*hBytes ];
                char *nextOut = outBuf ;
          
@@ -133,19 +136,15 @@ fprintf( stderr, "--> Printing something\n" );
                   }
                }
 
-               nextOut += sprintf( nextOut, "\x1d/%c\n\n\n\n", 0 );
+               nextOut += sprintf( nextOut, "\x1d/%c", 0 );
 
-               int const numWritten = write( fd, outBuf, nextOut-outBuf );
+               int const numWritten = write( printerFd_, outBuf, nextOut-outBuf );
                printf( "wrote %d bytes\n", numWritten );
          
-               write( fd, "\x1dV", 3 );
-
-               close( fd );
-               
                *rval = JSVAL_TRUE ;
             }
             else
-               JS_ReportError( cx, "%m opening printer" );
+               JS_ReportError( cx, "Invalid printer handle %d", printerFd_ );
             
          }
          else
@@ -160,23 +159,44 @@ fprintf( stderr, "--> Printing something\n" );
    return JS_TRUE ;
 }
 
+static JSBool
+jsCBMCut( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
+{
+fprintf( stderr, "--> CUT\n" );
+   *rval = JSVAL_FALSE ;
+
+   if( 0 <= printerFd_ )
+   {
+      printf( "printer port opened\n" );
+         
+      write( printerFd_, "\n\n\n\n\x1dV", 7 );
+      
+      *rval = JSVAL_TRUE ;
+   }
+   else
+      JS_ReportError( cx, "Invalid printer handle" );
+   
+   return JS_TRUE ;
+}
+
 static JSFunctionSpec cbm_methods[] = {
    { "print",        jsCBMPrint,      0,0,0 },
+   { "cut",          jsCBMCut,        0,0,0 },
    { 0 }
 };
 
 static JSBool jsCBM( JSContext *cx, JSObject *obj, uintN argc, jsval *argv, jsval *rval )
 {
+   *rval = JSVAL_FALSE ;
    obj = JS_NewObject( cx, &jsCBMClass_, NULL, NULL );
 
    if( obj )
    {
-fprintf( stderr, "--> printer object allocated\n" );
       *rval = OBJECT_TO_JSVAL(obj);
    }
    else
-      *rval = JSVAL_FALSE ;
-   
+      JS_ReportError( cx, "Error allocating printer" );
+
    return JS_TRUE;
 }
 
@@ -186,34 +206,64 @@ bool initJSCBM( JSContext *cx, JSObject *glob )
 {
 fprintf( stderr, "--> CBM...CBM...\n" );
 
-   JSObject *rval = JS_InitClass( cx, glob, NULL, &jsCBMClass_,
-                                  jsCBM, 1,
-                                  cbmProperties_, 
-                                  cbm_methods,
-                                  0, 0 );
-   if( rval )
+   int const fd = open( "/dev/usb/lp0", O_WRONLY );
+   if( 0 <= fd )
    {
-      JSObject *obj = JS_NewObject( cx, &jsCBMClass_, NULL, NULL );
-      if( obj )
+fprintf( stderr, "printer port opened: handle %d\n", fd );
+      printerFd_ = fd ;
+      JSObject *rval = JS_InitClass( cx, glob, NULL, &jsCBMClass_,
+                                     jsCBM, 1,
+                                     cbmProperties_, 
+                                     cbm_methods,
+                                     0, 0 );
+      if( rval )
       {
-         if( JS_DefineProperty( cx, glob, "printer", 
-                                OBJECT_TO_JSVAL( obj ),
-                                0, 0, 
-                                JSPROP_ENUMERATE
-                               |JSPROP_PERMANENT
-                               |JSPROP_READONLY ) )
+         JSObject *obj = JS_NewObject( cx, &jsCBMClass_, NULL, NULL );
+         if( obj )
          {
+            if( JS_DefineProperty( cx, glob, "printer", 
+                                   OBJECT_TO_JSVAL( obj ),
+                                   0, 0, 
+                                   JSPROP_ENUMERATE
+                                  |JSPROP_PERMANENT
+                                  |JSPROP_READONLY ) )
+            {
 fprintf( stderr, "--> Defined printer object\n" );
-            return true ;
+               return true ;
+            }
+            else
+               fprintf( stderr, "Error defining printer object\n" );
          }
          else
-            fprintf( stderr, "Error defining printer object\n" );
+            fprintf( stderr, "Error allocating CBM printer" );
       }
       else
-         fprintf( stderr, "Error allocating CBM printer" );
+         fprintf( stderr, "Error initializing CBM printer class\n" );
    }
    else
-      fprintf( stderr, "Error initializing CBM printer class\n" );
+      JS_ReportError( cx, "Error %m opening printer handle" );
+
    return false ;
+}
+
+bool closeCBM( JSContext *cx, JSObject *glob )
+{
+JS_ReportError( cx, "closing printer" );
+   jsval vPrinter ;
+   if( JS_GetProperty( cx, glob, "printer", &vPrinter ) 
+       &&
+       JSVAL_IS_OBJECT( vPrinter ) )
+   {
+      JSObject *rhObj = JSVAL_TO_OBJECT( vPrinter );
+      if( 0 <= printerFd_ )
+      {
+         close( printerFd_ );
+         return true ;
+      }
+      else
+         JS_ReportError( cx, "Invalid printer handle %d", printerFd_ );
+   }
+   else
+      JS_ReportError( cx, "Error getting printer object" );
 }
 
