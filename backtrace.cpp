21b2a31af1bc7be8c4ec8b6b9d8aa5a359b80136
ericn <ericn>
2003-06-08 15:20:29 +0000

    -additional detail

--- backtrace.cpp
+++ backtrace.cpp
@@ -1,53 +1,94 @@
-#include <iostream>
 
 #include <signal.h>
 #include <execinfo.h>
 #include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void handler(int aSignal, siginfo_t *aSigInfo, void *extraInfo)
+void handler( int sig )
 {
-        void *btArray[128];
-        int btSize;
-        char** btSymbols;
+   fprintf( stderr, "got signal %u\n", sig );
+   fprintf( stderr, "sighandler at %p\n", handler );
 
-        std::cout << std::endl << "########## Backtrace ##########" << std::endl;
-        btSize = backtrace(btArray, sizeof(btArray) / sizeof(void *));
-        std::cout << "Number of elements in backtrace: " <<  btSize << std::endl;
+   void *btArray[128];
+   int btSize = backtrace(btArray, sizeof(btArray) / sizeof(void *) );
+   
+   fprintf( stderr, "########## Backtrace ##########\n"
+                    "Number of elements in backtrace: %u\n", btSize );
 
-        if (btSize > 0) {
+   if (btSize > 0)
+   {
+      char** btSymbols = backtrace_symbols( btArray, btSize );
+      if( btSymbols )
+      {
+         for (int i = btSize - 1; i >= 0; --i)
+            fprintf( stderr, "%s\n", btSymbols[i] );
+      }
+   }
 
-            btSymbols = backtrace_symbols(btArray, btSize);
-            if (btSymbols) {
-                for (int i = btSize - 1; i >= 0; --i) {
-                    std::cout << btSymbols[i] << std::endl;
-                }
-            }
-        }
-
-        std::cout << "Handler done." << std::endl;
+   fprintf( stderr, "Handler done.\n" );
+   exit( 0 );
 }
 
-void g()
-{
-        abort();
-}
-
-void f()
-{
-        g();
-}
+static void f();
 
 int main(int argc, char **argv)
 {
-        struct sigaction SignalAction;
+   struct itimerval     itimer;
+   int                  interval = 1;   /* 1 second interval */
 
-        SignalAction.sa_sigaction = handler;
-        sigemptyset(&SignalAction.sa_mask);
-        SignalAction.sa_flags = SA_SIGINFO;
-        sigaction(SIGSEGV, &SignalAction, NULL);
-        sigaction(SIGABRT, &SignalAction, NULL);
+   /*
+    * Set up a signal handler for the SIGALRM signal which is what the
+    * expiring timer will set.
+    */
 
-        f();
+   signal (SIGALRM, handler);
+
+   /*
+    * Set the timer up to be non repeating, so that once it expires, it
+    * doesn't start another cycle.  What you do depends on what you need
+    * in a particular application.
+    */
+
+   itimer . it_interval . tv_sec = 0;
+   itimer . it_interval . tv_usec = 0;
+ 
+   /*
+    * Set the time to expiration to interval seconds.
+    * The timer resolution is milliseconds.
+    */
+   
+   itimer . it_value . tv_sec = interval;
+   itimer . it_value . tv_usec = 0;
+
+   if (setitimer (ITIMER_REAL, &itimer, NULL) < 0)
+   {
+      perror ("StartTimer could not setitimer");
+      exit (0);
+   }
+
+   f();
+
+   return 0 ;
 }
 
+
+static void h()
+{
+   while( 1 )
+      ;
+}
+
+static void g()
+{
+   h();
+}
+
+static void f()
+{
+   g();
+}
 
