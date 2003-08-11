291d364a558aa2ec93772023067518336b82f74b
ericn <ericn>
2003-08-11 00:10:43 +0000

    -prevent #include dependency

--- tickMs.h
+++ tickMs.h
@@ -1,5 +1,5 @@
 #ifndef __TICKMS_H__
-#define __TICKMS_H__ "$Id: tickMs.h,v 1.1 2003-07-27 15:19:12 ericn Exp $"
+#define __TICKMS_H__ "$Id: tickMs.h,v 1.2 2003-08-11 00:10:43 ericn Exp $"
 
 /*
  * tickMs.h
@@ -11,13 +11,18 @@
  * Change History : 
  *
  * $Log: tickMs.h,v $
- * Revision 1.1  2003-07-27 15:19:12  ericn
+ * Revision 1.2  2003-08-11 00:10:43  ericn
+ * -prevent #include dependency
+ *
+ * Revision 1.1  2003/07/27 15:19:12  ericn
  * -Initial import
  *
  *
  *
  * Copyright Boundary Devices, Inc. 2003
  */
+
+#include <sys/time.h>
 
 inline long long tickMs()
 {
