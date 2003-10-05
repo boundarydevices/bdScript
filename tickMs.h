a2211ef3b85beda4db5c646958b4f104d0394f92
ericn <ericn>
2003-10-05 19:11:37 +0000

    -added timeValToMs() call

--- tickMs.h
+++ tickMs.h
@@ -1,5 +1,5 @@
 #ifndef __TICKMS_H__
-#define __TICKMS_H__ "$Id: tickMs.h,v 1.2 2003-08-11 00:10:43 ericn Exp $"
+#define __TICKMS_H__ "$Id: tickMs.h,v 1.3 2003-10-05 19:11:37 ericn Exp $"
 
 /*
  * tickMs.h
@@ -11,7 +11,10 @@
  * Change History : 
  *
  * $Log: tickMs.h,v $
- * Revision 1.2  2003-08-11 00:10:43  ericn
+ * Revision 1.3  2003-10-05 19:11:37  ericn
+ * -added timeValToMs() call
+ *
+ * Revision 1.2  2003/08/11 00:10:43  ericn
  * -prevent #include dependency
  *
  * Revision 1.1  2003/07/27 15:19:12  ericn
@@ -24,12 +27,16 @@
 
 #include <sys/time.h>
 
+inline long long timeValToMs( struct timeval const &tv )
+{
+   return ((long long)tv.tv_sec*1000)+((long long)tv.tv_usec / 1000 );
+}
+
 inline long long tickMs()
 {
    struct timeval now ; gettimeofday( &now, 0 );
-   return ((long long)now.tv_sec*1000)+((long long)now.tv_usec / 1000 );
+   return timeValToMs( now );
 }
-
 
 #endif
 
