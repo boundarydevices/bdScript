0914813460e2efa376cf64a599edfe14921944cf
ericn <ericn>
2003-10-19 17:03:19 +0000

    -Initial import

---
+++ udpRecvAll.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+static unsigned const RXSIZE = 16384 ;
+
+int main( int argc, char const * const argv[] )
+{
+   if( 3 == argc )
+   {
+      struct in_addr localIP ;
+      if( inet_aton( argv[1], &localIP ) )
+      {
+         char *end ;
+         unsigned long localPort = strtoul( argv[2], &end, 0 );
+         if( ( 0 < localPort ) && ( 65536 > localPort )
+             &&
+             ( '\0' == *end ) )
+         {
+            printf( "IP %s, port 0x%04x\n", inet_ntoa( localIP ), localPort );
+            int const sFd = socket( AF_INET, SOCK_DGRAM, 0 );
+            if( 0 <= sFd )
+            {
+               sockaddr_in local ;
+               local.sin_family = AF_INET ;
+               local.sin_addr.s_addr = INADDR_ANY ;
+               local.sin_port   = localPort ;
+               if( 0 == bind( sFd, (struct sockaddr *) &local, sizeof( local ) ) )
+               {
+                  char * const rxBuf = new char [ RXSIZE ];
+                  while( 1 )
+                  {
+                     sockaddr_in remote ;
+                     socklen_t   remSize = sizeof( remote );
+                     int const numRx = recvfrom( sFd, rxBuf, RXSIZE, 0, 
+                                                 (struct sockaddr *)&remote, &remSize );
+                     if( numRx == RXSIZE )
+                     {
+//                        printf( "%u\n", numRx );
+                        printf( "%c", rxBuf[0] ); fflush( stdout );
+                     }
+                     else
+                     {
+                        fprintf( stderr, "rxErr %m" );
+                        break;
+                     }
+                  }
+               }
+               else
+                  fprintf( stderr, "bind:%m\n" );
+               close( sFd );
+            }
+            else
+               fprintf( stderr, "socketErr:%m\n" );
+         }
+         else
+            fprintf( stderr, "Invalid port <%s>\n", argv[2] );
+      }
+      else
+         fprintf( stderr, "Invalid ip <%s>\n", argv[1] );
+
+   }
+   else
+      fprintf( stderr, "usage: udpSpewTo ip(dotted decimal) port\n" );
+
+   return 0 ;
+}
---
+++ udpSpewTo.cpp
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+static unsigned const SENDSIZE = 16384 ;
+
+int main( int argc, char const * const argv[] )
+{
+   if( 3 == argc )
+   {
+      struct in_addr targetIP ;
+      if( inet_aton( argv[1], &targetIP ) )
+      {
+         char *end ;
+         unsigned long targetPort = strtoul( argv[2], &end, 0 );
+         if( ( 0 < targetPort ) && ( 65536 > targetPort )
+             &&
+             ( '\0' == *end ) )
+         {
+            printf( "IP %s, port 0x%04x\n", inet_ntoa( targetIP ), targetPort );
+            int const sFd = socket( AF_INET, SOCK_DGRAM, 0 );
+            if( 0 <= sFd )
+            {
+               sockaddr_in remote ;
+               remote.sin_family = AF_INET ;
+               remote.sin_addr   = targetIP ;
+               remote.sin_port   = targetPort ;
+
+               unsigned char * const sendBuf = new unsigned char [ SENDSIZE ];
+               char cNext = '0' ;
+               while( 1 )
+               {
+                  memset( sendBuf, cNext, SENDSIZE );
+                  int const numSent = sendto( sFd, sendBuf, SENDSIZE, 0, 
+                                              (struct sockaddr *)&remote, sizeof( remote ) );
+                  if( numSent == SENDSIZE )
+                  {
+                     printf( "t" ); fflush( stdout );
+                     cNext++ ;
+                     if( '9' < cNext )
+                        cNext = '0' ;
+
+                     struct timespec tv ;
+                     tv.tv_sec  = 0 ;
+                     tv.tv_nsec = 100000000 ;
+               
+               //      JS_ReportError( cx, "nanosleep %f : %u.%lu seconds\n", seconds, tv.tv_sec, tv.tv_nsec );
+               
+                     nanosleep( &tv, 0 );
+
+                  }
+                  else
+                  {
+                     fprintf( stderr, "sendToErr %m" );
+                     break;
+                  }
+               }
+               close( sFd );
+            }
+            else
+               fprintf( stderr, "socketErr:%m\n" );
+         }
+         else
+            fprintf( stderr, "Invalid port <%s>\n", argv[2] );
+      }
+      else
+         fprintf( stderr, "Invalid ip <%s>\n", argv[1] );
+
+   }
+   else
+      fprintf( stderr, "usage: udpSpewTo ip(dotted decimal) port\n" );
+
+   return 0 ;
+}
