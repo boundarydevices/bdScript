42eae041e17a883403c63eec6aa6f57ebc4b0512
ericn <ericn>
2004-05-08 23:54:45 +0000

    -expanded star_methods for pageHeight() call

--- jsStar.h
+++ jsStar.h
@@ -1,5 +1,5 @@
 #ifndef __JSSTAR_H__
-#define __JSSTAR_H__ "$Id: jsStar.h,v 1.1 2004-05-05 03:20:32 ericn Exp $"
+#define __JSSTAR_H__ "$Id: jsStar.h,v 1.2 2004-05-08 23:54:45 ericn Exp $"
 
 /*
  * jsStar.h
@@ -14,7 +14,10 @@
  * Change History : 
  *
  * $Log: jsStar.h,v $
- * Revision 1.1  2004-05-05 03:20:32  ericn
+ * Revision 1.2  2004-05-08 23:54:45  ericn
+ * -expanded star_methods for pageHeight() call
+ *
+ * Revision 1.1  2004/05/05 03:20:32  ericn
  * -Initial import
  *
  *
@@ -24,7 +27,7 @@
 
 #include "js/jsapi.h"
 
-extern JSFunctionSpec star_methods[2];
+extern JSFunctionSpec star_methods[3];
 
 void starPrinterFixup( JSContext *cx, 
                        JSObject  *obj );
