076b96122c11d85f86a5513b00c93f8b71507ad8
ericn <ericn>
2004-07-04 21:33:16 +0000

    -added debugHex() routine

--- debugPrint.h
+++ debugPrint.h
@@ -1,5 +1,5 @@
 #ifndef __DEBUGPRINT_H__
-#define __DEBUGPRINT_H__ "$Id: debugPrint.h,v 1.1 2003-11-24 19:42:42 ericn Exp $"
+#define __DEBUGPRINT_H__ "$Id: debugPrint.h,v 1.2 2004-07-04 21:33:16 ericn Exp $"
 
 /*
  * debugPrint.h
@@ -12,7 +12,10 @@
  * Change History : 
  *
  * $Log: debugPrint.h,v $
- * Revision 1.1  2003-11-24 19:42:42  ericn
+ * Revision 1.2  2004-07-04 21:33:16  ericn
+ * -added debugHex() routine
+ *
+ * Revision 1.1  2003/11/24 19:42:42  ericn
  * -polling touch screen
  *
  *
@@ -23,17 +26,27 @@
 #ifdef DEBUGPRINT
 #include <stdio.h>
 #include <stdarg.h>
+#include "hexDump.h"
 
 inline int debugPrint( char const *fmt, ... )
 {
    va_list ap;
    va_start( ap, fmt );
-   return vprintf( fmt, ap );
+   return vfprintf( stdout, fmt, ap );
+}
+
+inline void debugHex( char const *label, void const *data, unsigned size )
+{
+   dumpHex( label, data, size );
 }
 
 #else
 
-inline int debugPrint( char const *fmt, ... )
+inline int debugPrint( char const *, ... )
+{
+}
+
+inline void debugHex( char const *, void const *, unsigned )
 {
 }
 
