e017a133589c6c9cdd4a92c047d16f97be2d9ad1
ericn <ericn>
2004-12-03 04:43:50 +0000

    -use median/mean filters

--- touchPoll.cpp
+++ touchPoll.cpp
@@ -8,7 +8,10 @@
  * Change History : 
  *
  * $Log: touchPoll.cpp,v $
- * Revision 1.6  2004-11-26 15:28:54  ericn
+ * Revision 1.7  2004-12-03 04:43:50  ericn
+ * -use median/mean filters
+ *
+ * Revision 1.6  2004/11/26 15:28:54  ericn
  * -use median/mean instead of range filter
  *
  * Revision 1.5  2004/01/01 20:11:42  ericn
@@ -36,6 +39,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+
+
+// #define DEBUGPRINT
 #include "debugPrint.h"
 #include "rollingMedian.h"
 #include "rollingMean.h"
@@ -90,21 +96,28 @@ static bool wasDown = false ;
 
 void touchPoll_t :: onDataAvail( void )
 {
-   ts_event event ;
    ts_event nextEvent ;
+   ts_event event ;
    unsigned count = 0 ;
    int numRead ;
 
    while( sizeof( nextEvent ) == ( numRead = read( fd_, &nextEvent, sizeof( nextEvent ) ) ) )
    {
-      medianX_.feed( event.x );
-      medianY_.feed( event.y );
-      unsigned short sample ;
-      if( medianX_.read( sample ) )
-         meanX_.feed( sample );
-      if( medianY_.read( sample ) )
-         meanY_.feed( sample );
+      medianX_.feed( nextEvent.x );
+      medianY_.feed( nextEvent.y );
 
+debugPrint( "sample: %u/%u\n", nextEvent.x, nextEvent.y );
+#ifdef XDEBUGPRINT
+      printf( "medianX: " ); medianX_.dump();
+      printf( "medianY: " ); medianY_.dump();
+#endif
+
+      unsigned short sampleX ;
+      if( medianX_.read( sampleX ) )
+         meanX_.feed( sampleX );
+      unsigned short sampleY ;
+      if( medianY_.read( sampleY ) )
+         meanY_.feed( sampleY );
       event = nextEvent ;
       count++ ;
    }
@@ -114,13 +127,15 @@ void touchPoll_t :: onDataAvail( void )
       if( 0 < event.pressure )
       {
          unsigned short x, y ;
+//         x = event.x ; y = event.y ; if( 1 )
 //         if( medianX_.read( x ) && medianY_.read( y ) )
          if( meanX_.read( x ) && meanY_.read( y ) )
          {
-            onTouch( event.x, event.y, event.pressure, event.stamp );
+debugPrint( "out: %u/%u\n", x, y );
+            onTouch( x, y, event.pressure, event.stamp );
             if( !wasDown )
             {
-debugPrint( "first touch at %u:%u\n", event.x, event.y );
+debugPrint( "first touch at %u:%u\n", x, y );
                wasDown = true ;
             }
          }
