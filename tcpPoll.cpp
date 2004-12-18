5f309c9916937bdc5bbbc15cefb858fdfc3905fd
ericn <ericn>
2004-12-18 18:29:14 +0000

    -added dns support

--- tcpPoll.cpp
+++ tcpPoll.cpp
@@ -8,7 +8,10 @@
  * Change History : 
  *
  * $Log: tcpPoll.cpp,v $
- * Revision 1.2  2004-03-17 04:56:19  ericn
+ * Revision 1.3  2004-12-18 18:29:18  ericn
+ * -added dns support
+ *
+ * Revision 1.2  2004/03/17 04:56:19  ericn
  * -updates for mini-board (no sound, video, touch screen)
  *
  * Revision 1.1  2004/02/08 10:34:24  ericn
@@ -29,38 +32,87 @@
 #include <stdio.h>
 #include <sys/errno.h>
 #include <sys/ioctl.h>
+#include <arpa/inet.h>
+#include "dnsPoll.h"
+#define DEBUGPRINT
 #include "debugPrint.h"
-
-tcpClient_t :: ~tcpClient_t( void )
-{
-}
-
-void tcpClient_t :: onConnect( tcpHandler_t & ){ debugPrint( "tcpClient_t::onConnect" ); }
-void tcpClient_t :: onDisconnect( tcpHandler_t & ){ debugPrint( "tcpClient_t::onDisconnect" ); }
-
+#include <stdlib.h>
 
 tcpHandler_t :: tcpHandler_t
    ( unsigned long     serverIP,          // network byte order
      unsigned short    serverPort,        // network byte order
-     pollHandlerSet_t &set,
-     tcpClient_t      *client )
+     pollHandlerSet_t &set )
    : pollHandler_t( socket( AF_INET, SOCK_STREAM, 0 ), set )
-   , serverIP_( serverIP )
+   , hostName_( 0 )
+   , serverIP_( 0 )
    , serverPort_( serverPort )
    , state_( closed )
-   , clients_( 0 )
 {
+   memset( callbacks_, 0, sizeof( callbacks_ ) );
    if( 0 < getFd() )
    {
       fcntl( getFd(), F_SETFD, FD_CLOEXEC );
       fcntl( getFd(), F_SETFL, O_NONBLOCK );
-      
+
+      state_ = nameLookup ;
+      struct in_addr in ;
+      in.s_addr = serverIP ;
+      onNameLookup( inet_ntoa( in ), true, serverIP );
+   }
+}
+
+static void nameLookupShim( void       *cbParam,
+                            char const *hostName,
+                            bool        resolved,
+                            unsigned long address )
+{
+   ((tcpHandler_t *)cbParam)->onNameLookup( hostName, resolved, address );
+}
+
+tcpHandler_t :: tcpHandler_t
+   ( char const       *hostName,
+     unsigned short    serverPort,        // network byte order
+     pollHandlerSet_t &set )
+   : pollHandler_t( socket( AF_INET, SOCK_STREAM, 0 ), set )
+   , serverIP_( 0 )
+   , serverPort_( serverPort )
+   , state_( closed )
+{
+   memset( callbacks_, 0, sizeof( callbacks_ ) );
+   if( 0 < getFd() )
+   {
+      fcntl( getFd(), F_SETFD, FD_CLOEXEC );
+      fcntl( getFd(), F_SETFL, O_NONBLOCK );
+
+      state_ = nameLookup ;
+      dnsPoll_t::get().getHostByName( hostName, 
+                                      nameLookupShim, 
+                                      this,
+                                      4000 );
+   }
+}
+
+void tcpHandler_t :: onNameLookup
+   ( char const   *hostName, 
+     bool          resolved,
+     unsigned long address )
+{
+   if( hostName_ )
+      free( (void *)hostName_ );
+   if( hostName )
+      hostName_ = strdup( hostName );
+   else
+      hostName_ = 0 ;
+
+   if( resolved )
+   {
+      serverIP_ = address ;
       sockaddr_in serverAddr ;
       memset( &serverAddr, 0, sizeof( serverAddr ) );
       
       serverAddr.sin_family      = AF_INET;
-      serverAddr.sin_addr.s_addr = serverIP ;
-      serverAddr.sin_port        = serverPort ;
+      serverAddr.sin_addr.s_addr = address ;
+      serverAddr.sin_port        = serverPort_ ;
    
       int const connectResult = connect( getFd(), (struct sockaddr *) &serverAddr, sizeof( serverAddr ) );
       if( ( 0 == connectResult )
@@ -71,7 +123,7 @@ tcpHandler_t :: tcpHandler_t
          setsockopt( getFd(), SOL_SOCKET, TCP_NODELAY, &doit, sizeof( doit ) );
          state_ = connecting ;
          setMask( POLLIN|POLLOUT|POLLERR|POLLHUP );
-         set.add( *this );
+         parent_.add( *this );
          if( 0 == connectResult )
          {
             state_ = connected ;
@@ -83,59 +135,42 @@ tcpHandler_t :: tcpHandler_t
          close();
       }
    }
-
-   if( client )
-      addClient( *client );
+   else
+   {
+      if( 0 != callbacks_[error_e] )
+         callbacks_[dataAvail_e]( *this, error_e, cbParams_[error_e] );
+   }
 }
 
 tcpHandler_t :: ~tcpHandler_t( void )
 {
-   while( clients_ )
-   {
-      client_t *old = clients_ ;
-      clients_ = old->next_ ;
-      delete old ;
-   }
+   if( hostName_ )
+      free( (void *)hostName_ );
 
    parent_.removeMe( *this );
    close();
 }
 
-void tcpHandler_t :: addClient( tcpClient_t &client )
+void tcpHandler_t :: setCallback
+   ( event_e       event, 
+     tcpCallback_t callback,
+     void         *opaque )
 {
-   removeClient( client ); // no duplicates!
-
-   client_t *newClient = new client_t ;
-   newClient->client_ = &client ;
-   newClient->next_   = clients_ ;
-   clients_ = newClient ;
-   if( connected == getState() )
-      client.onConnect( *this );
-   else if( closed == getState() )
-      client.onDisconnect( *this );
+   callbacks_[event] = callback ;
+   cbParams_[event]  = opaque ;
 }
 
-void tcpHandler_t :: removeClient( tcpClient_t &client )
+tcpCallback_t tcpHandler_t :: getCallback( event_e event )
 {
-   client_t *rear = 0 ;
-   client_t *front = clients_ ;
-   while( front )
-   {
-      if( front->client_ == &client )
-      {
-         if( 0 == rear )
-            clients_ = front->next_ ;
-         else
-            rear->next_ = front->next_ ;
-         delete front ;
-         front = 0 ; // terminate loop
-      }
-      else
-      {
-         rear = front ;
-         front = rear->next_ ;
-      }
-   }
+   return callbacks_[event];
+}
+
+tcpCallback_t tcpHandler_t :: clearCallback( event_e event )
+{
+   tcpCallback_t old = callbacks_[event];
+   callbacks_[event] = 0 ;
+   cbParams_[event]  = 0 ;
+   return old ;
 }
 
 void tcpHandler_t :: onDataAvail( void )     // POLLIN
@@ -145,12 +180,11 @@ void tcpHandler_t :: onDataAvail( void )     // POLLIN
    {
       unsigned short const prevMask = getMask();
       unsigned short mask = prevMask & ~POLLIN ;
-      client_t *cl = clients_ ;
-      while( cl )
+      if( ( 0 != callbacks_[dataAvail_e] )
+          &&
+          callbacks_[dataAvail_e]( *this, dataAvail_e, cbParams_[dataAvail_e] ) )
       {
-         cl->client_->onDataAvail( *this );
-         mask |= ( cl->client_->getMask() & POLLIN );
-         cl = cl->next_ ;
+         mask |= POLLIN ;
       }
 
       if( mask != prevMask )
@@ -165,12 +199,11 @@ void tcpHandler_t :: onWriteSpace( void )    // POLLOUT
    {
       unsigned short const prevMask = getMask();
       unsigned short mask = prevMask & ~POLLOUT ;
-      client_t *cl = clients_ ;
-      while( cl )
+      if( ( 0 != callbacks_[writeSpaceAvail_e] )
+          &&
+          callbacks_[writeSpaceAvail_e]( *this, writeSpaceAvail_e, cbParams_[writeSpaceAvail_e] ) )
       {
-         cl->client_->onWriteSpace( *this );
-         mask |= ( cl->client_->getMask() & POLLOUT );
-         cl = cl->next_ ;
+         mask |= POLLOUT ;
       }
 
       if( mask != prevMask )
@@ -181,23 +214,19 @@ void tcpHandler_t :: onWriteSpace( void )    // POLLOUT
 void tcpHandler_t :: onError( void )         // POLLERR
 {
    checkStatus();
-   client_t *cl = clients_ ;
-   while( cl )
-   {
-      cl->client_->onError( *this );
-      cl = cl->next_ ;
-   }
+   if( 0 != callbacks_[error_e] )
+      callbacks_[error_e]( *this, error_e, cbParams_[error_e] );
 }
 
 void tcpHandler_t :: onHUP( void )           // POLLHUP
 {
-   client_t *cl = clients_ ;
-   while( cl )
-   {
-      cl->client_->onDisconnect( *this );
-      cl = cl->next_ ;
-   }
+   checkStatus();
+   if( ( connected == state_ )
+       &&
+       ( 0 != callbacks_[disconnect_e] ) )
+      callbacks_[disconnect_e]( *this, disconnect_e, cbParams_[disconnect_e] );
    close();
+   setMask(0);
    state_ = closed ;
 }
 
@@ -211,12 +240,8 @@ void tcpHandler_t :: checkStatus( void )
    {
       if( connecting == state_ )
       {
-         client_t *cl = clients_ ;
-         while( cl )
-         {
-            cl->client_->onConnect( *this );
-            cl = cl->next_ ;
-         }
+         if( 0 != callbacks_[connect_e] )
+            callbacks_[connect_e]( *this, connect_e, cbParams_[connect_e] );
          state_ = connected ;
       }
    }
@@ -225,12 +250,8 @@ void tcpHandler_t :: checkStatus( void )
       printf( "closed:err 0x%x\n", err );
       close();
       state_ = closed ;
-      client_t *cl = clients_ ;
-      while( cl )
-      {
-         cl->client_->onDisconnect( *this );
-         cl = cl->next_ ;
-      }
+      if( 0 != callbacks_[disconnect_e] )
+         callbacks_[disconnect_e]( *this, disconnect_e, cbParams_[disconnect_e] );
       setMask( 0 );
    }
 }
@@ -242,53 +263,17 @@ void tcpHandler_t :: checkStatus( void )
 #include "ttyPoll.h"
 #include "memFile.h"
 
-class ttyClient_t : public tcpClient_t {
-public:
-   ttyClient_t( char const * const inData,
-                unsigned long numBytes );
-   virtual ~ttyClient_t( void );
-   virtual void onConnect( tcpHandler_t & );
-   virtual unsigned short getMask( void );
-   virtual void onDataAvail( pollHandler_t & );     // POLLIN
-   virtual void onWriteSpace( pollHandler_t & );    // POLLOUT
-   virtual void onError( pollHandler_t & );         // POLLERR
-   virtual void onHUP( pollHandler_t & );           // POLLHUP
-   virtual void onDisconnect( tcpHandler_t & );
-
-   char const *  inData_ ;
-   unsigned long numBytes_ ;    
-};
-
-ttyClient_t :: ttyClient_t
-   ( char const * const inData,
-     unsigned long numBytes )
-   : inData_( inData )
-   , numBytes_( numBytes )
-{
-}
-
-ttyClient_t :: ~ttyClient_t( void )
-{
-}
-
-void ttyClient_t :: onConnect( tcpHandler_t & )
+bool onConnect( tcpHandler_t &handler, int event, void *opaque )
 {
    printf( "Connected!\n" );
+   return true ;
 }
 
-unsigned short ttyClient_t :: getMask( void )
-{
-   unsigned mask = POLLIN | POLLERR | POLLHUP ;
-   if( 0 < numBytes_ )
-      mask |= POLLOUT ;
-   return mask ;
-}
-
-void ttyClient_t :: onDataAvail( pollHandler_t &h )     // POLLIN
+bool onDataAvail( tcpHandler_t &handler, int event, void *opaque )
 {
    printf( "dataAvail\n" );
    int numAvail ;
-   int const ioctlResult = ioctl( h.getFd(), FIONREAD, &numAvail );
+   int const ioctlResult = ioctl( handler.getFd(), FIONREAD, &numAvail );
    if( ( 0 == ioctlResult )
        &&
        ( 0 < numAvail ) )
@@ -298,59 +283,92 @@ printf( "%u bytes\n", numAvail );
       char inData[2048];
       if( sizeof( inData ) < numAvail )
          numAvail = sizeof( inData );
-      int const numRead = read( h.getFd(), inData, numAvail );
+      int const numRead = read( handler.getFd(), inData, numAvail );
       if( 0 < numRead )
       {
          write( fileno( stdout ), inData, numRead );
          fflush( stdout );
       }
-      return ;
    }
    else if( 0 == ioctlResult )
    {
 printf( "?? closed ??\n" );
-      h.onHUP();
+      handler.onHUP();
    }
    else
       perror( "FIONREAD" );
+   return true ;
 }
 
-void ttyClient_t :: onWriteSpace( pollHandler_t &h )    // POLLOUT
+bool onWriteSpace( tcpHandler_t &handler, int event, void *opaque )
 {
    printf( "writeSpace\n" );
-   if( 0 < numBytes_ )
-   {
-      int const numWritten = write( h.getFd(), inData_, numBytes_ );
-      if( 0 < numWritten )
-      {
-         numBytes_ -= numWritten ;
-         inData_   += numWritten ;
-      }
-      else
-         printf( "write error: %d:%m\n", numWritten );
-   }
-   else
-      printf( "nothing to write\n" );
+   return false ;
 }
 
-void ttyClient_t :: onError( pollHandler_t & )         // POLLERR
+bool onError( tcpHandler_t &handler, int event, void *opaque )
 {
    printf( "Error\n" );
+   return true ;
 }
 
-void ttyClient_t :: onHUP( pollHandler_t & )           // POLLHUP
+bool onHUP( tcpHandler_t &handler, int event, void *opaque )
 {
    printf( "HUP\n" );
+   return true ;
 }
 
-void ttyClient_t :: onDisconnect( tcpHandler_t & )
+bool onDisconnect( tcpHandler_t &handler, int event, void *opaque )
 {
-   printf( "Disconnected!\n" );
+   struct in_addr in ;
+   in.s_addr = handler.getServerIp();
+   printf( "Disconnected from server %s, ip %s, port %u\n", 
+           handler.hostName(), 
+           inet_ntoa( in ), 
+           ntohs( handler.getServerPort() ) );
 }
 
 #include <signal.h>
 #include "hexDump.h"
 #include <execinfo.h>
+#include "ttyPoll.h"
+
+static bool doExit_ = false ;
+
+class myTTY_t : public ttyPollHandler_t {
+public:
+   myTTY_t( pollHandlerSet_t  &set,
+            tcpHandler_t      &handler )
+      : ttyPollHandler_t( set, 0 ), handler_( handler ){}
+   virtual void onLineIn( void );
+   virtual void onCtrlC( void );
+
+   tcpHandler_t &handler_ ;
+};
+
+void myTTY_t :: onLineIn( void )
+{
+   printf( "ttyIn: %s\n", getLine() );
+   char const *sIn = getLine();
+   if( *sIn )
+   {
+      unsigned const len = strlen( sIn );
+      printf( "--> %s..", sIn );
+      int numWritten = write( handler_.getFd(), sIn, len )
+                     + write( handler_.getFd(), "\r\n", 2 );
+      printf( "%d bytes\n", numWritten );
+   }
+   else
+   {
+      printf( "state 0x%02x\n", handler_.getState() );
+   }
+}
+
+void myTTY_t :: onCtrlC( void )
+{
+   printf( "<ctrl-C>\n" );
+   doExit_ = true ;
+}
 
 static struct sigaction sa;
 static struct sigaction oldint;
@@ -394,39 +412,50 @@ int main( int argc, char const * const argv[] )
    printf( "Hello, %s\n", argv[0] );
    if( 1 < argc )
    {
-      in_addr server ;
-      if( 0 != inet_aton( argv[1], &server ) )
+      unsigned short port = htons( 80 );
+      if( 2 < argc )
+         port = htons( (unsigned short)strtoul( argv[2], 0, 0 ) );
+      
+      printf( "polling IP adress %s, port 0x%04x\n", argv[1], port );
+
+      memFile_t fIn( "in.dat" );
+      char const *inData = 0 ;
+      unsigned    numBytes = 0 ;
+      if( fIn.worked() )
       {
-         unsigned short port = htons( 80 );
-         if( 2 < argc )
-            port = htons( (unsigned short)strtoul( argv[2], 0, 0 ) );
-         printf( "polling IP adress %s:%lx, port 0x%04x\n", argv[1], server.s_addr, port );
-         
-         memFile_t fIn( "in.dat" );
-         char const *inData = 0 ;
-         unsigned    numBytes = 0 ;
-         if( fIn.worked() )
-         {
-            inData = (char const *)fIn.getData();
-            numBytes = fIn.getLength();
-         }
-         else
-            printf( "no input data\n" );
-         
-         pollHandlerSet_t fds ;
-         ttyClient_t tty( inData, numBytes );
-         printf( "tty constructed\n" );
-         tcpHandler_t tcp( server.s_addr, port, fds, &tty );
-         printf( "tcp constructed\n" );
-         while( tcp.closed != tcp.getState() )
-         {
-            fds.poll( 1000 );
-            printf( "state 0x%02x\n", tcp.getState() );
-         }
-         tcp.removeClient( tty );
+         inData = (char const *)fIn.getData();
+         numBytes = fIn.getLength();
       }
       else
-         fprintf( stderr, "invalid IP address <%s>\n", argv[1] );
+         printf( "no input data\n" );
+         
+      pollHandlerSet_t fds ;
+      getTimerPoll( fds );
+      dnsPoll_t &dns = dnsPoll_t::get( fds );
+
+      printf( "client constructed\n" );
+      in_addr server ;
+      tcpHandler_t *pTCP ;
+      if( 0 != inet_aton( argv[1], &server ) )
+         pTCP = new tcpHandler_t( server.s_addr, port, fds );
+      else
+         pTCP = new tcpHandler_t( argv[1], port, fds );
+
+      tcpHandler_t &tcp = *pTCP ;
+      printf( "tcp constructed\n" );
+      myTTY_t myTTY( fds, tcp );
+      printf( "tty constructed\n" );
+
+      tcp.setCallback( tcpHandler_t::connect_e, onConnect, 0 );
+      tcp.setCallback( tcpHandler_t::disconnect_e, onDisconnect, 0 );
+      tcp.setCallback( tcpHandler_t::dataAvail_e, onDataAvail, 0 );
+      tcp.setCallback( tcpHandler_t::writeSpaceAvail_e, onWriteSpace, 0 );
+      tcp.setCallback( tcpHandler_t::error_e, onError, 0 );
+   
+      while( !doExit_ && ( tcp.closed != tcp.getState() ) )
+      {
+         fds.poll( 1000 );
+      }
    }
    else
       fprintf( stderr, "Usage: tcpPoll server [port=80]\n" );
--- tcpPoll.h
+++ tcpPoll.h
@@ -1,5 +1,5 @@
 #ifndef __TCPPOLL_H__
-#define __TCPPOLL_H__ "$Id: tcpPoll.h,v 1.1 2004-02-08 10:34:22 ericn Exp $"
+#define __TCPPOLL_H__ "$Id: tcpPoll.h,v 1.2 2004-12-18 18:29:14 ericn Exp $"
 
 /*
  * tcpPoll.h
@@ -7,13 +7,17 @@
  * This header file declares the tcpPollHandler_t classes, which
  * is used to make a connection to a TCP server for HTTP, etc.
  *
+ * Note that this class doesn't make too many assumptions about
+ * what's done with the data, it simply makes callbacks no
+ *
  * Change History : 
  *
  * $Log: tcpPoll.h,v $
- * Revision 1.1  2004-02-08 10:34:22  ericn
+ * Revision 1.2  2004-12-18 18:29:14  ericn
+ * -added dns support
+ *
+ * Revision 1.1  2004/02/08 10:34:22  ericn
  * -Initial import
- *
- *
  *
  * Copyright Boundary Devices, Inc. 2004
  */
@@ -24,32 +28,57 @@
 
 class tcpHandler_t ;
 
-
-class tcpClient_t : public pollClient_t {
-public:
-   virtual ~tcpClient_t( void );
-   virtual void onConnect( tcpHandler_t & );
-   virtual void onDisconnect( tcpHandler_t & );
-};
+//
+// Return value should be true if we should keep polling
+// for dataAvail and writeSpaceAvail events, or false to
+// ignore the signal (clear the POLL mask)
+//
+typedef bool (*tcpCallback_t)( tcpHandler_t &tcpHandler,
+                               int           event,        // tcpHandler_t::event_e
+                               void         *opaque );
 
 
 class tcpHandler_t : public pollHandler_t {
 public:
+   tcpHandler_t( char const       *hostName,
+                 unsigned short    serverPort,        // network byte order
+                 pollHandlerSet_t &set );
    tcpHandler_t( unsigned long     serverIP,          // network byte order
                  unsigned short    serverPort,        // network byte order
-                 pollHandlerSet_t &set,
-                 tcpClient_t      *client = 0 );
-   virtual ~tcpHandler_t( void );
+                 pollHandlerSet_t &set );
+   virtual ~tcpHandler_t( void );             
 
    enum state_e {
+      nameLookup,
       connecting,
       connected,
       closed
    };
 
+   enum event_e {
+      nameResolved_e,
+      connect_e,
+      disconnect_e,
+      dataAvail_e,
+      writeSpaceAvail_e,
+      error_e,
+      numEvents
+   };
+
    state_e getState( void ) const { return state_ ; }
-   void addClient( tcpClient_t &client );
-   void removeClient( tcpClient_t &client );
+   
+   void setCallback( event_e       event, 
+                     tcpCallback_t callback,
+                     void         *opaque );
+   tcpCallback_t getCallback( event_e event );
+   tcpCallback_t clearCallback( event_e event );
+
+   void onNameLookup( char const   *hostName, 
+                      bool          resolved,
+                      unsigned long address );
+   unsigned long getServerIp() const { return serverIP_ ; }
+   unsigned short getServerPort() const { return serverPort_ ; }
+   char const *hostName( void ) const { return hostName_ ? hostName_ : "unknown" ; }
 
    //
    // ---------------------------------------------------------------
@@ -64,17 +93,13 @@ public:
    void checkStatus( void );
 
 protected:
-   struct client_t {
-      tcpClient_t *client_ ;
-      client_t    *next_ ;
-   };
-   
-   unsigned long const  serverIP_ ;
+   char const          *hostName_ ;
+   unsigned long        serverIP_ ;
    unsigned short const serverPort_ ;
    state_e              state_ ;
-   client_t            *clients_ ;
+   void                *cbParams_[numEvents];
+   tcpCallback_t        callbacks_[numEvents];
 };
-
 
 #endif
 
