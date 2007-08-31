c6204adf2be65e382ac1f8ba6414fb1cb0e24f2e
ericn <ericn>
2007-08-31 01:11:39 +0000

    -change to recvfrom() instead of recv()

--- udpRx.cpp
+++ udpRx.cpp
@@ -10,18 +10,21 @@ int main( void )
    sockaddr_in local ;
    local.sin_family      = AF_INET ;
    local.sin_addr.s_addr = INADDR_ANY ;
-   local.sin_port        = 0xBDBD ;
+   local.sin_port        = 0xBDBD;
    bind( fd, (struct sockaddr *)&local, sizeof( local ) );
    int doit = 1 ;
    setsockopt( fd, SOL_SOCKET, SO_BROADCAST, &doit, sizeof( doit ) );
 
    char inBuf[512];
    int numRead ;
-   while( 0 < ( numRead = recv( fd, inBuf, sizeof(inBuf), 0 ) ) )
+   sockaddr_in rem ;
+   socklen_t remSize = sizeof(rem);
+   while( 0 < ( numRead = recvfrom( fd, inBuf, sizeof(inBuf), 0, (struct sockaddr *)&rem, &remSize ) ) )
    {
       printf( "rx %u bytes\n", numRead );
       fwrite( inBuf, numRead, 1, stdout );
       printf( "\n" );
+
    }
 
    return 0 ;
