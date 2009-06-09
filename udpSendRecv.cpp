e73e4ee96f77cb69bd5c740d8f1e3185acf8de8a
ericn <ericn>
2009-06-09 21:46:01 +0000

    allow escaped inputs

--- udpSendRecv.cpp
+++ udpSendRecv.cpp
@@ -12,7 +12,77 @@
 
 static unsigned const SENDSIZE = 16384 ;
 
-int main( int argc, char const * const argv[] )
+static void unescape( char     *s,        // in+out
+                      unsigned &length )  // out
+{
+   enum state_t {
+      normal,
+      backslash,
+      hex1,
+      hex2
+   };
+
+   char *const start = s ;
+   char *nextOut = start ;
+   char c ;
+   state_t state = normal ;
+
+   while( 0 != (c = *s++) ){
+      switch( state ){
+         case normal: {
+            if( '\\' == c ){
+               state = backslash ;
+            }
+            else 
+               *nextOut++ = c ;
+            break ;
+         }
+         case backslash: {
+            if( 'n' == c ){
+               *nextOut++ = '\n' ;
+            }
+            else if( 'r' == c ){
+               *nextOut++ = '\r' ;
+            }
+            else if( '\\' == c ){
+               *nextOut++ = '\\' ;
+            }
+            else if( 'x' == c ){
+               state = hex1 ;
+            }
+            break ;
+         }
+         case hex1: {
+            if( ('0' <= c) && ('9' >= c) )
+               *nextOut = (c-'0')<< 4;
+            else if( ('a' <= c) && ('f' >= c) )
+               *nextOut = (c-'a'+10)<< 4;
+            else if( ('A' <= c) && ('F' >= c) )
+               *nextOut = (c-'A'+10)<< 4;
+            else
+               state = normal ;
+            if( state != normal )
+               state = hex2 ;
+            break ;
+         }
+         case hex2: {
+            if( ('0' <= c) && ('9' >= c) )
+               *nextOut++ += c-'0' ;
+            else if( ('a' <= c) && ('f' >= c) )
+               *nextOut++ += c-'a'+10 ;
+            else if( ('A' <= c) && ('F' >= c) )
+               *nextOut++ += c-'a'+10 ;
+            state = normal ;
+            break ;
+         }
+      }
+   }
+
+   *nextOut = 0 ;
+   length = nextOut-start ;
+}
+
+int main( int argc, char *argv[] )
 {
    if( 4 == argc )
    {
@@ -25,7 +95,7 @@ int main( int argc, char const * const argv[] )
              &&
              ( '\0' == *end ) )
          {
-            printf( "IP %s, port 0x%04x\n", inet_ntoa( targetIP ), targetPort );
+            printf( "IP %s, port 0x%04lx\n", inet_ntoa( targetIP ), targetPort );
             int const sFd = socket( AF_INET, SOCK_DGRAM, 0 );
             if( 0 <= sFd )
             {
@@ -34,12 +104,13 @@ int main( int argc, char const * const argv[] )
                remote.sin_addr   = targetIP ;
                remote.sin_port   = htons(targetPort) ;
 
-               if( '@' != argv[3][0] )
+               if( '^' != argv[3][0] )
                {
-                  int const len = strlen( argv[3] );
+		  unsigned len ;
+		  unescape(argv[3],len);
                   int const numSent = sendto( sFd, argv[3], len, 0, 
                                               (struct sockaddr *)&remote, sizeof( remote ) );
-                  if( numSent == len )
+                  if( numSent == (int)len )
                      printf( "sent %d bytes\n", numSent );
                   else
                      perror( "sendto" );
@@ -65,8 +136,8 @@ int main( int argc, char const * const argv[] )
 
                pollfd  fds_[2];
                fds_[0].fd = sFd ;
-               fds_[0].revents = POLLIN | POLLERR | POLLHUP ;
-               int numReady = ::poll( fds_, 1, 1000 );
+               fds_[0].events = POLLIN ;
+               int numReady = ::poll( fds_, 1, 10000 );
                if( 0 <= numReady ){
                   char inBuf[512];
                   sockaddr_in fromAddr ;
@@ -78,8 +149,9 @@ int main( int argc, char const * const argv[] )
                      inBuf[numRead] = 0 ;
                      printf( "read %u bytes <%s>\n", numRead, inBuf );
                   }
-                  else
-                     perror( "read()" );
+		  else {
+                     perror( "recvfrom()" );
+		  }
                }
                else
                   perror( "poll()" );
