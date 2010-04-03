c77c47af8d243e9c7f3afed4f1fadf532693e763
Eric Nelson <ericn@ericsony.(none)>
2010-04-03 08:45:18 -0700

    [udpRx] Allow broadcast, port re-use

--- udpRx.cpp
+++ udpRx.cpp
@@ -13,7 +13,13 @@ int main( void )
    local.sin_port        = 0xBDBD;
    bind( fd, (struct sockaddr *)&local, sizeof( local ) );
    int doit = 1 ;
-   setsockopt( fd, SOL_SOCKET, SO_BROADCAST, &doit, sizeof( doit ) );
+   int result = setsockopt( fd, SOL_SOCKET, SO_BROADCAST, &doit, sizeof( doit ) );
+   if( 0 != result )
+	   perror( "SO_BROADCAST" );
+   doit = 1 ;
+   result = setsockopt( fd, SOL_SOCKET, SO_REUSEADDR, &doit, sizeof( doit ) );
+   if( 0 != result )
+      fprintf( stderr, "SO_REUSEADDR:%d:%m\n", result );
 
    char inBuf[512];
    int numRead ;
@@ -21,7 +27,7 @@ int main( void )
    socklen_t remSize = sizeof(rem);
    while( 0 < ( numRead = recvfrom( fd, inBuf, sizeof(inBuf), 0, (struct sockaddr *)&rem, &remSize ) ) )
    {
-      printf( "rx %u bytes\n", numRead );
+      printf( "rx %u bytes from %s, port 0x%x\n", numRead, inet_ntoa(rem.sin_addr), ntohs(rem.sin_port) );
       fwrite( inBuf, numRead, 1, stdout );
       printf( "\n" );
 
